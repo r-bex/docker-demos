4486c636e069bcb745348aca41ec76d8d8139996
Rebecca Davey <rebecca.davey@netacea.com>
2020-11-21 17:19:13 +0000

    predictor to predict language of tweets in stream

--- multi_container_demo/predictor/predict.py
+++ multi_container_demo/predictor/predict.py
@@ -1,15 +1,70 @@
-from utils import make_kafka_consumer, get_kafka_logger
-import logging
+import json
 import os
-import time
+import random
+import preprocessor as p
+
+from utils import make_kafka_consumer, get_logger
 
 KAFKA_ADDRESS = os.environ["kafka_server"] + ":" + os.environ["kafka_port"]
 KAFKA_TOPIC = os.environ["kafka_topic"]
 
-if __name__ == "__main__":
-    logger = get_kafka_logger()
 
+def preprocess_tweet(tweet_string):
+    # apply preprocessor library to remove tags, urls etc
+    cleaned_string = p.clean(tweet_string)
+    # remove punctuation
+    for punc_to_remove in [".",",","!","?"]:
+        cleaned_string = cleaned_string.replace(punc_to_remove, "")
+    # additionally, everything lowercase and  remove any words that aren't entirely alphanumeric
+    words = [w.lower() for w in cleaned_string.split(" ") if w.isalpha()]
+    return " ".join(words)
+
+
+class TweetLanguageClassifier():
+    def __init__(self, language_words):
+        self.corpus = self._load_corpus(language_words)
+
+    def _load_corpus(self, raw_words):
+        # bytes to string and strip newlines
+        return [w.decode("utf-8").replace("\n", "") for w in raw_words]
+
+    def predict(self, tweet_text, prob_threshold=0.5):
+        if len(tweet_text) == 0:
+            return -1
+
+        tweet_words = tweet_text.split(" ")
+        language_counts = [int(tw in self.corpus) for tw in tweet_words]
+        language_probability = sum(language_counts) / len(tweet_words)
+
+        if language_probability >= prob_threshold:
+            return 1
+        else:
+            return 0
+
+
+if __name__ == "__main__":
+    logger = get_logger()
+
+    # read in list of common words and use to init Classifier
+    with open("corpus.txt", "rb") as f:
+        common_words = f.readlines()
+    f.close()
+    classifier = TweetLanguageClassifier(common_words)
+
+    # set up a Kafka consumer
     consumer = make_kafka_consumer(KAFKA_ADDRESS, KAFKA_TOPIC, logger)
 
     for message in consumer:
-        logger.info(message)
+        message_data = json.loads(message.value.decode("utf-8"))
+        logger.info("Raw text: {}".format(message_data["data"]["text"]))
+
+        cleaned_text = preprocess_tweet(message_data["data"]["text"])
+        logger.info("Cleaned text: {}".format(cleaned_text))
+
+        prediction = classifier.predict(cleaned_text)
+        prediction_text_map = {
+            1: "English",
+            0: "not English",
+            -1: "n/a"
+        }
+        logger.info("Prediction: {} ({})".format(prediction, prediction_text_map[prediction]))
--- multi_container_demo/predictor/utils.py
+++ multi_container_demo/predictor/utils.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer, KafkaProducer
 import logging
 import time
 
+
 def connect_to_kafka(connection_type, address, topic=None, logger=None):
     """
     """
@@ -16,7 +17,8 @@ def connect_to_kafka(connection_type, address, topic=None, logger=None):
 
     while not connected_to_kafka:
         try:
-            logger.info("connecting to kafka @ {} attempt #{}".format(address, str(attempted_connections)))
+            logger.info("connecting to kafka @ {} attempt #{}".format(
+                address, str(attempted_connections)))
 
             if connection_type == "consumer":
                 connection = KafkaConsumer(topic, bootstrap_servers=[address])
@@ -28,23 +30,30 @@ def connect_to_kafka(connection_type, address, topic=None, logger=None):
         except Exception as e:
             if attempted_connections < 5:
                 attempted_connections += 1
-                logger.warning("Failed to connect to kafka at '{}', retrying in 5 seconds (attempt {})".format(address, attempted_connections))
+                logger.warning(
+                    "Failed to connect to kafka at '{}', retrying in 5 seconds (attempt {})"
+                    .format(address, attempted_connections))
                 logger.warning(str(e))
                 time.sleep(5)
             else:
-                logger.error("Failed to connect to kafka at '{}' after {} retries, exiting".format(address, attempted_connections))
+                logger.error(
+                    "Failed to connect to kafka at '{}' after {} retries, exiting"
+                    .format(address, attempted_connections))
                 break
 
     assert connection, "Couldn't create a kafka producer. Exiting"
     return connection
 
+
 def make_kafka_consumer(address, topic, logger):
     return connect_to_kafka("consumer", address, topic=topic, logger=logger)
+
 
 def make_kafka_producer(address, logger):
     return connect_to_kafka("producer", address, topic=None, logger=logger)
 
-def get_kafka_logger():
+
+def get_logger():
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("kafka-logger")
     return logger
