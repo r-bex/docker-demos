f26fc3a2c9799b3692702e206769bfa59a88704f
Rebecca Davey <rebecca.davey@netacea.com>
2020-11-21 17:18:29 +0000

    kafka producer sends tweet data

--- multi_container_demo/data/producer.py
+++ multi_container_demo/data/producer.py
@@ -1,40 +1,32 @@
-import datetime as dt
-from utils import make_kafka_producer, get_kafka_logger
-import json
-import logging
 import os
+import random
 import time
+
+from utils import make_kafka_producer, get_logger
 
 KAFKA_ADDRESS = os.environ["kafka_server"] + ":" + os.environ["kafka_port"]
 KAFKA_TOPIC = os.environ["kafka_topic"]
 
-def parse_line(lb):
-    # decode bytes to string, chop of \r\n at back and split by tab delimiter
-    line_parts = lb.decode("utf-8")[:-2].split("\t")
-    timestamp_str = " ".join(line_parts[0:2])
-    return str.encode(json.dumps({
-        "timestamp": timestamp_str,
-        "temperature": float(line_parts[2])
-    }))
-
 if __name__ == "__main__":
-    logger = get_kafka_logger()
-    
+    logger = get_logger()
+
     # load file of temperatures
-    with open("temp_data.txt", "rb") as f:
+    with open("sample_tweets.json", "rb") as f:
         lines = f.readlines()
     f.close()
 
+    # shuffle the order
+    random.shuffle(lines)
+
     # create Kafka producer
     producer = make_kafka_producer(KAFKA_ADDRESS, logger=logger)
-    
+
     # print a new line every second
-    for line in lines:
-        producer.send(
-            KAFKA_TOPIC,
-            parse_line(line), 
-            timestamp_ms=int(round(time.time() * 1000))
-        )
-        logger.info(f"Sent message to {KAFKA_TOPIC}")
-        time.sleep(1)
-    
+    while True:
+        for line in lines:
+            producer.send(KAFKA_TOPIC,
+                          line,
+                          timestamp_ms=int(round(time.time() * 1000)))
+            logger.info("Sent message to %s", KAFKA_TOPIC)
+            seconds_to_wait = random.randint(1, 5)
+            time.sleep(seconds_to_wait)
