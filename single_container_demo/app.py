ba4e9235bd179f3d65bca4dd694d1b529a3cb5fe
Rebecca Davey <rebecca.davey@netacea.com>
2020-11-24 20:58:15 +0000

    formatting?

--- single_container_demo/app.py
+++ single_container_demo/app.py
@@ -10,21 +10,28 @@ logger.setLevel("INFO")
 
 STDEV_THRESHOLD = os.environ.get("stdev_threshold", 1)
 
+
 def get_data_from_last_n_hours(num_hours):
-    run_timestamp = dt.datetime.now().replace(minute=0, second=0, microsecond=0)
+    run_timestamp = dt.datetime.now().replace(minute=0,
+                                              second=0,
+                                              microsecond=0)
     values = []
-    for minute in range(0, 60*num_hours):
+    for minute in range(0, 60 * num_hours):
         ts = run_timestamp - dt.timedelta(minutes=minute)
         values.append((ts, random.random()))
     return values[::-1]
+
 
 def detect_anomalies(value_data):
     values = [value for (minute, value) in value_data]
     avg_val = np.mean(values)
     stdev = np.std(values)
 
-    labelled_values = [(minute, value, abs(value - avg_val) > stdev*STDEV_THRESHOLD) for (minute, value) in value_data]
+    labelled_values = [(minute, value,
+                        abs(value - avg_val) > stdev * STDEV_THRESHOLD)
+                       for (minute, value) in value_data]
     return labelled_values
+
 
 def write_out_predictions(labelled_data):
     only_anom = [(ts, val) for (ts, val, anom) in labelled_data if anom]
